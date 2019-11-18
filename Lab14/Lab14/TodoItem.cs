bee52f9fa6d9b9c38796d1ced91e918607beed67
Jeanlui21 <jean.ferrer@tecsup.edu.pe>
2019-11-18 15:31:20 -0500

    Agregar archivos de proyecto.

---
+++ Lab14/Lab14/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Lab14
+{
+   public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int DNI { get; set; }
+        public string nombres{ get; set; }
+        public string apellidos { get; set; }
+        public DateTime fecha { get; set; }
+        public bool Done { get; set; }
+    }
+}
